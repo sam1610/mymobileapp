7f2e4241bd3af792122ccb4b0c6cf315b8270976
sam1610 <samir.amri@gmail.com>
2018-12-10 22:47:38 +0300

    contacts Image Added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,7 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
  contacts =" Constacts Application ";
+ contactsImg="../../assets/imgs/contactsImg.jpg";
   constructor(public navCtrl: NavController) {
 
   }
