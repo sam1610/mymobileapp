418ee8b893ba5f940e092dd55065cde65e46d024
sam1610 <samir.amri@gmail.com>
2018-12-11 00:22:04 +0300

    array of contacts added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+// home.ts
 import { AboutPage } from './../about/about';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
@@ -10,8 +11,12 @@ export class HomePage {
  contacts =" Constacts Application ";
  contactsImg="../../assets/imgs/contactsImg.jpg";
  userName= "";
+ contactsArray=[
+  {Name:"Ahmed", id:1},
+  {Name:"samir", id:2},
+  {Name:"Ali", id:3}
+ ];
   constructor(public navCtrl: NavController) {
-
   }
   aboutContacts(){
     this.navCtrl.push(AboutPage);
