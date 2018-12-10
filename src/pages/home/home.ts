871a930c9e962a77e0e1cc449e461821cc7ec35e
sam1610 <samir.amri@gmail.com>
2018-12-10 22:38:52 +0300

    contact Added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+ contacts =" Constacts Application ";
   constructor(public navCtrl: NavController) {
 
   }
