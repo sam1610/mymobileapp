c196730afcc47b651741530c461da8ed7b8c9271
sam1610 <samir.amri@gmail.com>
2019-01-31 13:34:37 +0300

    getContacts()

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,14 +11,16 @@ export class HomePage {
  contacts =" Constacts Application ";
  contactsImg="../../assets/imgs/contactsImg.jpg";
  userName= "";
- contactsArray=[
-  {Name:"Ahmed", id:1},
-  {Name:"samir", id:2},
-  {Name:"Ali", id:3}
- ];
   constructor(public navCtrl: NavController) {
   }
   aboutContacts(){
     this.navCtrl.push(AboutPage);
+  }
+  getContacts(){
+    return  [
+      {Name:"Ahmed", id:1},
+      {Name:"samir", id:2},
+      {Name:"Ali", id:3}
+     ]
   }
 }
