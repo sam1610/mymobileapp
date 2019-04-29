bb2512c08f6a07c7dcdb8e097cb49b3946a48fee
sam1610 <samir.amri@gmail.com>
2019-04-29 12:01:33 +0300

    getlocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,14 +11,20 @@ export class HomePage {
  contacts =" Constacts Application ";
  contactsImg="../../assets/imgs/contactsImg.jpg";
  userName= "";
- countriesArray=[
-  {"country": "bahrain", "city": "manama" },
-  {"country": "FL", "city": "miami"},
-  {"country": "KSA", "city": "riyad"  },
-  {"country": "Egypt", "city": "Cairo"  }
-];
+ 
   constructor(public navCtrl: NavController) {
   }
   aboutContacts(){
     this.navCtrl.push(AboutPage);}
+
+  getLoaction(){
+return [
+  {"country": "bahrain", "city": "manama" },
+  {"country": "FL", "city": "miami"},
+  {"country": "KSA", "city": "riyad"  },
+  {"country": "Egypt", "city": "Cairo"  }
+]
+
+  }
+
   }
